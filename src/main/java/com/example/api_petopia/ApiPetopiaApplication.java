62ddb132aa0b8ae5440a4636b2a453e364adc17f
Đạt.Code <datyasuo202@gmail.com>
2022-09-19 20:32:56 +0700

    first commit

---
+++ src/main/java/com/example/api_petopia/ApiPetopiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_petopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPetopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPetopiaApplication.class, args);
+    }
+
+}
