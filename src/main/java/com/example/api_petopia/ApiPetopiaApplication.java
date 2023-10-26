b971a7722d813edad480db5feb8113f5860d1389
quynhnga30 <93904468+quynhnga30@users.noreply.github.com>
2023-10-26 23:09:51 +0700

    init

--- src/main/java/com/example/api_petopia/ApiPetopiaApplication.java
+++ src/main/java/com/example/api_petopia/ApiPetopiaApplication.java
@@ -2,6 +2,10 @@ package com.example.api_petopia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class ApiPetopiaApplication {
@@ -9,5 +13,10 @@ public class ApiPetopiaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiPetopiaApplication.class, args);
     }
+    @Bean(name = "bCryptPasswordEncoder")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
